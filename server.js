0710524af28e034aede24c8b86d5444050cff595
jizhang80 <zhangji80@outlook.com>
2023-10-31 18:00:19 -0400

    re-org program's logic, put all the function to objects, let controller handle action

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
+const inquirer = require("inquirer");
 const welcome = require("./lib/tools.js");
-const {rootQ, departmentsQ, rolesQ, employeesQ, qList, askQuestion} = require("./lib/questions.js");
-const Controller = require("./lib/controller.js");
-
-const ctlr = new Controller();
+const questions = require("./lib/questions.js");
+const handler = require("./lib/controller.js");
 
 async function main() {
-    while(ctlr.state !== 'quit') {
-        const answers = await askQuestion(qList(ctlr.state));
-        ctlr.handler(answers);
+    welcome("This is CMS system for employee information.")
+    while(true) {
+        await inquirer
+        .prompt(questions)
+        .then(answers => handler(answers))
+        .catch(err => console.error(err));
     }
 }
 
-
+main();
