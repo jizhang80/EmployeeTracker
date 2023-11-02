5082884fa5e58c27e5e474104cb5f09852cbfbde
jizhang80 <zhangji80@outlook.com>
2023-11-02 10:18:38 -0400

    add db scripts

---
+++ lib/db/seeds.sql
@@ -0,0 +1,35 @@
+-- department data
+INSERT INTO department (name) 
+VALUES ("Chef"),
+        ("Development"),
+        ("Sales"),
+        ("Operation");
+
+-- role data
+INSERT INTO role (title, salary, department_id) 
+VALUES ("CEO", 1, 1),
+       ("CFO", 1, 1),
+       ("Manager", 5000, 2),
+       ("Band10", 5000, 2),
+       ("Band9", 4000, 2),
+       ("Band8", 3000, 2),
+       ("Manager", 5000, 3),
+       ("Band10", 4000, 3),
+       ("Band9", 3000, 3),
+       ("Band8", 2000, 3),
+       ("Manager", 5000, 4),
+       ("Band10", 4000, 4),
+       ("Band9", 3000, 4),
+       ("Band8", 2000, 4);
+
+-- employee data
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Jimmy", "ZHANG", 1, NULL),
+        ("Amanda", "M.", 2, NULL),
+        ("Amy", "G.", 3, 1),
+        ("Bobby", "T.", 4, 3),
+        ("Cindy", "L.", 7, 1),
+        ("Jennifer", "L.", 8, 5),
+        ("Bill", "G.", 11, 2),
+        ("Elen", "S.", 12, 7),
+        ("Damian", "F.", 13, 7);
